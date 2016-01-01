d592b616409881ae57bc73a439f8bdea157061ef
Heesang Chae <theand@gmail.com>
2016-01-01 22:20:50 +0900

    ruby minitest boilerplate

---
+++ ruby-minitest-boilerplate/README.md
@@ -0,0 +1,9 @@
+
+
+
+# minitest 유닛테스트
+
+bundle => gem 설치 확인
+
+rake => ./test 폴더에 있는 테스트 케이스 실행.
+
---
+++ ruby-minitest-boilerplate/test/test_helper.rb
@@ -0,0 +1,19 @@
+require 'bundler/setup'
+
+require 'minitest/autorun'
+
+require 'stringio'
+
+module Kernel
+
+def capture
+  out = StringIO.new
+  $stdout = out
+  yield
+  return out
+
+  ensure
+    $stdout = STDOUT
+  end
+
+end
